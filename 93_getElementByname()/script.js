6ccd41503211280d4147d58d951f7f211bba7755
Shruti Karmukale <shrutikarmukale@gmail.com>
2026-08-05 09:36:32 +0000

    getElementByName

---
+++ 93_getElementByname()/script.js
@@ -0,0 +1,9 @@
+let fruits = document.getElementsByClassName("fruit");
+
+console.log(fruits);
+
+console.log(fruits[0].innerHTML);
+
+for(let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i].innerHTML);
+}
