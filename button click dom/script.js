e5b96d09446911c3b1ac5a5d98ae354981865196
Shruti Karmukale <shrutikarmukale@gmail.com>
2026-08-06 09:46:18 +0000

    button click

---
+++ button click dom/script.js
@@ -0,0 +1,14 @@
+let title=document.getElementById("title");
+let btn=document.getElementById("btn");
+
+btn.onclick=function(){
+
+title.innerHTML="Welcome";
+
+title.style.color="blue";
+
+title.style.fontSize="40px";
+
+title.classList.add("red");
+
+};
